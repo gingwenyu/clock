90980608daf3a7a5808872d430a22e6621fe616d
gingwenyu <gingwenyu830208@gmail.com>
2019-03-27 19:17:19 +0800

    fix hourHand

--- JS/all.js
+++ JS/all.js
@@ -11,21 +11,20 @@ let minuteHand = document.querySelector('#minute');
 let hour;
 let hourDeg;
 let hourHand = document.querySelector('#hour');
+
 function timer() {
   let d = new Date();  //儲存當下日期時間等資料
   second = d.getSeconds();
   secondDeg = ((second / 60) * 360);  // 利用秒數計算出指針的角度   
-  secondHand.style.transform = `rotate(${secondDeg}deg)`;  // 將資料圖像化    
+  secondHand.style.transform = `rotate(${secondDeg}deg)`;  // 將資料圖像化 
+
   minute = d.getMinutes();
-  minuteDeg = ((minute / 60) * 360);
+  minuteDeg = ((minute / 60) * 360) + ((second / 60) * 6);
   minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
 
   hour = d.getHours();
-  hourDeg = ((hour / 12) * 360);
+  hourDeg = ((hour / 12) * 360) + ((minute / 60) * 30); 
   hourHand.style.transform = `rotate(${hourDeg}deg)`;
-  //時針移動方式 待修改
+
+  document.querySelector('#txtclock').textContent = d;  
 }
-
-
-
-
